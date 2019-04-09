f9564d55f578c2d8d093e69a61e2c27bb9848f34
Tom Vidler <ctgaminga@gmail.com>
2019-04-09 01:34:08 +0000

    Started trying to scan & store a string...

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
-#include <std.io>
+#include <stdio.h>
+
+/*--------
+Cipher algorithms (currently only rotation encoding)
+--------*/
 
 int main() {
-   printf("Hello World");
+   //char message[100];                    //initialise an array of length 10, used to remember our string
+   int x;
+   
+   printf("What is your message?\n");   //ask user what they would like to encode
+   scanf("%d", &x);                //
+   printf("%d", x);
    return 0;
 }
