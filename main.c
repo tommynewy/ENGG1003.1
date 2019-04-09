e6658e2c343f183b0b0714078442dd1e139ac3c5
Tom Vidler <ctgaminga@gmail.com>
2019-04-09 02:28:35 +0000

    Added input file and tried reading

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
-/*--------
+/*-----------------
 Cipher algorithms (currently only rotation encoding)
---------*/
+
+User instructions: read & use the terminal using the below commands:
+cd /projects/ENGG1500.1 ls
+./a.out
+------------------*/
 
 int main() {
-   //char message[100];                    //initialise an array of length 10, used to remember our string
-   int x;
-   
-   printf("What is your message?\n");   //ask user what they would like to encode
-   scanf("%d", &x);                //
-   printf("%d", x);
+   char message[1024];                  //initialise an array of length 1024, used to remember our string
+   FILE *input = fopen("input.txt", "r");
+   fgets(message, 1024, input);
+//   printf("What is your message?\n");   //ask user what they would like to encode
+   printf("You gave: %s\n", message);
    return 0;
 }
