cc5ea4b548732715fcd46d0e50c5da249d928ced
Tom Vidler <ctgaminga@gmail.com>
2019-04-23 05:27:44 +0000

    still trying to work the basics

--- main.c
+++ main.c
@@ -4,25 +4,27 @@
 Cipher algorithms (currently only rotation encoding)
 
 User instructions: read & use the terminal using the below commands:
-cd /projects/ENGG1500.1 ls
+cd /projects/ENGG1500.1 
+ls
 ./a.out
 ------------------*/
 
 int main() {
-   char Imessage[1024];                  //initialise an array of length 1024, used to remember our string
-   char Omessage[1024];                  //
-   FILE *input = fopen("input.txt", "r");
-   FILE *output = fopen("output.txt", "w");
-   int n = 1, key = 0;
-   
-   fscanf(input, "%c", Imessage);
+    char Imessage[1024];                  //initialise an array of length 1024, used to remember our string
+    char Omessage[1024];                  //
+    //FILE *input = fopen("input.txt", "r");
+    //FILE *output = fopen("output.txt", "w");
+    int n = 1, key = 0;
+    printf("What is your message? ");
+    scanf("%s\n", Imessage);
+   //fscanf(input, "%c", Imessage);
 //   printf("What is your message?\n");   //ask user what they would like to encode
-   if (key = 0) {
+   /*if (key = 0) {
        Omessage == Imessage;
-   }
+   }*/
    
    printf("You gave: %s\n", Imessage);
    
-   fprintf(Omessage, Omessage, output);
+   //fprintf(Omessage, Omessage, output);
    return 0;
 }
