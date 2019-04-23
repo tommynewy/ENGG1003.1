10b1ec29b03cd03a20f69232a7058f0f1cbf22cb
Tom Vidler <ctgaminga@gmail.com>
2019-04-23 08:00:39 +0000

    Implemented  loop to encode

--- main.c
+++ main.c
@@ -10,21 +10,42 @@ ls
 ------------------*/
 
 int main() {
-    char Imessage[1024];                  //initialise an array of length 1024, used to remember our string
-    char Omessage[1024];                  //
+    char Imessage[100];                  //initialise an array of length 1024, used to remember our string
+    char Omessage[100];                  //
     //FILE *input = fopen("input.txt", "r");
     //FILE *output = fopen("output.txt", "w");
-    int n = 1, key = 0;
-    printf("What is your message? ");
-    scanf("%s\n", Imessage);
+    int n = 0, key = 0;
+    
+    printf("What is your message?\n");
+    scanf("%c", Imessage);
    //fscanf(input, "%c", Imessage);
 //   printf("What is your message?\n");   //ask user what they would like to encode
-   /*if (key = 0) {
-       Omessage == Imessage;
-   }*/
+   
+    while (n <= 100) {
+        if (key = 0) {
+            Omessage[n] = Imessage[n];
+            break;
+        } else if (Imessage[n] <= 90 - key) {
+            Omessage[n] = Imessage[n] + key;
+            break;
+        } else if (Imessage[n] > 90 - key) {
+            Omessage[n] = 65 + (key - (90 - Imessage[n]));
+            break;
+        } else if (Imessage[n] < 122 - key) {
+            Omessage[n] = Imessage[n] - 32 + key;
+            break;
+        } else if (Imessage[n] > 122 - key) {
+            Omessage[n] = 68 + (122 - Imessage[n]);
+            break;
+        } else {
+            Omessage[n] = Imessage[n];
+            break;
+        }
+        ++n;
+    }
    
    printf("You gave: %s\n", Imessage);
    
-   //fprintf(Omessage, Omessage, output);
+   printf("Encoded as: %s\n", Omessage);
    return 0;
 }
